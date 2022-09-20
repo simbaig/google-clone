e760f5433d612aea9a8e99933c1c4e9a8bb4d016
wasim-ay <wasimbaig000@gmail.com>
2022-09-21 01:23:36 +0530

    created basic layout

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+	return (
+		<div className="text-center mt-10 p-10 border-t dark:border-gray-700 border-gray-200">
+			<h1>2022 Google, Inc.</h1>
+		</div>
+	);
+};
+
+export default Footer;
